fdd03bda535d34b9c39249adafa1e38c715e568e
samreachyan <samreachyan@gmail.com>
2021-02-14 18:12:04 +0700

    ProductScreen

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -12,6 +12,7 @@ const Header = () => {
           </LinkContainer>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className="ml-auto">
               <LinkContainer to='/cart'>
                 <Nav.Link>
                   <i className='fas fa-shopping-cart'></i> Cart
@@ -22,6 +23,7 @@ const Header = () => {
                   <i className='fas fa-user'></i> Sign In
                 </Nav.Link>
               </LinkContainer>
+            </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
