3a9730cb2d938c0441645200b18ec73331d16747
samreachyan <samreachyan@gmail.com>
2021-02-16 01:07:18 +0700

    init redux

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,30 +1,34 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { listProduct } from '../actions/productActions'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
-import axios from 'axios'
 
 const HomeScreen = () => {
-    const [products, setProducts] = useState([])
+    const dispatch = useDispatch()
+    const productList = useSelector((state) => state.productList)
+    const { loading, error, products } = productList
 
     useEffect(() => {
-        const fetchProducts = async() => {
-            const { data } = await axios.get('/api/products')
-            
-            setProducts(data);
-        }
+        dispatch(listProduct())
+    }, [dispatch])
 
-        fetchProducts()
-    },[])
     return (
         <>
             <h1>Latest Products</h1>
-            <Row>
+            {loading ? (
+                <h2>Loading...</h2>
+            ) : error ? (
+                <h3>{error}</h3>
+            ) : (
+                <Row>
                 {products.map((product) => (
                     <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                        <Product product={product} />
+                    <Product product={product} />
                     </Col>
                 ))}
-            </Row>
+                </Row>
+            )}
         </>
     )
 }
