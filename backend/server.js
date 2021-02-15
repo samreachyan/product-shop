d341434e4f8cdd66c3a35aa4786e87ce9a0bdd82
samreachyan <samreachyan@gmail.com>
2021-02-16 00:28:19 +0700

    middleware

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,8 @@ const cors = require('cors')
 const dotenv = require('dotenv')
 const colors = require('colors')
 const connectDB = require('./config/db')
-const products = require('./data/products')
+const productRoutes = require('./routes/productRoutes')
+const { notFound, errorHandler } = require('./middleware/errorMiddleware')
 
 dotenv.config()
 
@@ -16,14 +17,10 @@ app.get('/', (req, res) => {
     res.send('API is running...')
 })
 
-app.get('/api/products', (req, res) => {
-    res.json(products)
-})
+app.use('/api/products', productRoutes)
 
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find((p) => p._id === req.params.id)
-    res.json(product)
-})
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
