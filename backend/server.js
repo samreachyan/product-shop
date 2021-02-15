890adbdc5785ff4cdb48c576bbe389eaee7d20ec
samreachyan <samreachyan@gmail.com>
2021-02-15 18:03:50 +0700

    connect db

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const cors = require('cors')
+const dotenv = require('dotenv')
+const colors = require('colors')
+const connectDB = require('./config/db')
+const products = require('./data/products')
+
+dotenv.config()
+
+connectDB()
+
+const app = express()
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send('API is running...')
+})
+
+app.get('/api/products', (req, res) => {
+    res.json(products)
+})
+
+app.get('/api/products/:id', (req, res) => {
+    const product = products.find((p) => p._id === req.params.id)
+    res.json(product)
+})
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`).yellow.bold)
