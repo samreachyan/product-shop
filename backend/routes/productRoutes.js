1bc08bc01fd1f9e09e5d477bf2f362f01f554f6c
samreachyan <samreachyan@gmail.com>
2021-02-19 00:51:41 +0700

    product routes

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,11 +1,13 @@
 const express = require('express')
-const { getProducts, getProductById, deleteProduct } = require('../controller/productController')
+const { getProducts, getProductById, deleteProduct, updateProduct } = require('../controller/productController')
 const { protect, admin } = require('../middleware/authMiddleware')
 const router = express.Router()
 
 router.route('/').get(getProducts)
 router.route('/:id')
         .get(getProductById)
+        .delete(protect, admin, deleteProduct)
+        .put(protect, admin, updateProduct)
         .delete(protect, admin, deleteProduct)
 
 module.exports = router
